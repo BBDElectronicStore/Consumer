02371f550e41f0f39d96f07b57444c4ac5f308ba
AdrianHawkins <Adrian.Hawkins@bbd.co.za>
2024-06-25 21:01:06 +0200

    approve all order!

--- src/services/bank.service.ts
+++ src/services/bank.service.ts
@@ -3,6 +3,6 @@ import {BankDetails} from "../interfaces/messages/bankDetails";
 export class BankService {
     async processPayment(totalCost: number, bankDetails: BankDetails) {
         console.log('Processing payment');
-        return false;
+        return true;
     }
 }
