2ab0cacbe95aa8bee628bfd7f5206bf1daa7f7e3
AdrianHawkins <Adrian.Hawkins@bbd.co.za>
2024-06-26 19:26:04 +0200

    Pend order while witing on bank

--- src/consumers/payment.consumer.ts
+++ src/consumers/payment.consumer.ts
@@ -8,6 +8,7 @@ import {DenyOrderCommand} from "../commands/denyOrder.command";
 import {OrderRepository} from "../repositories/order.repository";
 import {FulfilOrderCommand} from "../commands/fulfilOrderCommand";
 import {GetStatusQuery} from "../queries/getStatus.query";
+import {PendOrderCommand} from "../commands/pendOrder.command";
 
 
 export class PaymentConsumer implements IConsume<payment, payment> {
@@ -22,15 +23,9 @@ export class PaymentConsumer implements IConsume<payment, payment> {
         }
         const bankService = new BankService();
         Logger.debug(`[${message.orderRef}] processing payment for order`);
-        const paymentSuccess = await bankService.processPayment(message.totalCost, message.bankDetails);
-        if(paymentSuccess) {
-            const command = new FulfilOrderCommand(new OrderRepository());
-            await command.execute(message.orderRef);
-        }
-        else {
-            const command = new DenyOrderCommand(new OrderRepository());
-            await command.execute(message.orderRef);
-        }
+        await bankService.processPayment(message.totalCost, message.orderRef, message.bankDetails);
+        const command = new PendOrderCommand(new OrderRepository());
+        await command.execute(message.orderRef);
     }
 
     @Handle(PaymentMessage.err)
--- src/services/bank.service.ts
+++ src/services/bank.service.ts
@@ -1,7 +1,7 @@
 import {BankDetails} from "../interfaces/messages/bankDetails";
 
 export class BankService {
-    async processPayment(totalCost: number, bankDetails: BankDetails) {
+    async processPayment(totalCost: number, orderRef: string, bankDetails: BankDetails) {
         console.log('Processing payment');
         return true;
     }
